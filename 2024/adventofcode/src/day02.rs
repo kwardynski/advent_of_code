99761c7feaac8748c0c57211d4bfd43b06665db2
kwardynski <kacper.wardynski@gmail.com>
2024-12-22 09:17:12 -0700

    get ready

--- 2024/adventofcode/src/day02.rs
+++ 2024/adventofcode/src/day02.rs
@@ -1,8 +1,9 @@
-use csv::ReaderBuilder;
+// use csv::ReaderBuilder;
 
-const FILE_NAME: &str = "../inputs/Day02.csv";
+// const FILE_NAME: &str = "../inputs/Day02.csv";
 
 pub fn solution() {
+    println!("\nDay 2")
 
     // We want to read line by line
 
