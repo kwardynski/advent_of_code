fe47137b56683fa63778c8c3ebfcbe1cce996abb
kwardynski <kacper.wardynski@gmail.com>
2025-01-05 14:56:02 -0700

    abstract fns

--- 2024/adventofcode/src/day02.rs
+++ 2024/adventofcode/src/day02.rs
@@ -3,42 +3,58 @@ use std::io::{BufRead, BufReader};
 
 const FILE_NAME: &str = "../inputs/Day02.csv";
 
+fn parse_line(line: &String) -> Vec<i32> {
+    let mut line_vector: Vec<i32> = Vec::new();
+
+    for string_int in line.split_whitespace() {
+        let int = string_int.parse::<i32>().unwrap();
+        line_vector.push(int);
+    }
+
+    return line_vector;
+}
+
+fn calculate_diffs(vector: &Vec<i32>) -> Vec<i32> {
+    let mut diff_vector: Vec<i32> = Vec::new();
+
+    for window in vector.windows(2) {
+        let diff = window[1] - window[0];
+        diff_vector.push(diff);
+    }
+
+    return diff_vector;
+}
+
+fn is_report_safe(vector: &Vec<i32>) -> bool {
+    let all_positive: bool = vector.into_iter().all(|x| x.is_positive());
+    let all_negative: bool = vector.into_iter().all(|x| !x.is_positive());
+    let all_steps_safe: bool = vector
+        .into_iter()
+        .all(|x| ((x.abs() >= 1) && (x.abs() <= 3)));
+
+    return (all_positive || all_negative) && all_steps_safe;
+}
+
 pub fn solution() {
     println!("\nDay 2");
 
+    // Part 1
     // - Open the file
     // - For each line:
     //     - Split the lne at the space
     //     - Convert each word into a string, stuff it in a vector
     //     - Check if the vector is constantly increasing / decreasing
     //     - Check if the values in the vector are all off by 1 - 3
-
     let mut num_safe_lines = 0;
     let reader = BufReader::new(File::open(FILE_NAME).expect("Cannot open file"));
 
     for line in reader.lines() {
-        let mut line_vector: Vec<i32> = Vec::new();
+        let line_vector = parse_line(&line.unwrap());
+        let diff_vector = calculate_diffs(&line_vector);
+        let safe = is_report_safe(&diff_vector);
 
-        for string_int in line.unwrap().split_whitespace() {
-            let int = string_int.parse::<i32>().unwrap();
-            line_vector.push(int);
-        }
-
-        let mut diff_vector: Vec<i32> = Vec::new();
-        for window in line_vector.windows(2) {
-            let diff = window[1] - window[0];
-            diff_vector.push(diff);
-        }
-
-        let all_positive = diff_vector.clone().into_iter().all(|x| x.is_positive());
-        let all_negative = diff_vector.clone().into_iter().all(|x| !x.is_positive());
-        let all_safe = diff_vector
-            .clone()
-            .into_iter()
-            .all(|x| ((x.abs() >= 1) && (x.abs() <= 3)));
-
-        if (all_positive || all_negative) && all_safe {
-            num_safe_lines = num_safe_lines + 1;
+        if safe {
+            num_safe_lines = num_safe_lines + 1
         }
     }
 
